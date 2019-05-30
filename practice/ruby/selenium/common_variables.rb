ecca84422de2d6b4a73a9bbe22bcfb2c5a1db03f
Oleksii Didilov <odidi@softserveinc.com>
2019-05-30 17:59:33 +0300

    Add module "Service" and test case for it
    Module "Service" was created to allow users start, stop and check
    status of service.
    Test for module "Service" was created to make sure that given module
    works properly.

--- practice/ruby/selenium/common_variables.rb
+++ practice/ruby/selenium/common_variables.rb
@@ -3,7 +3,6 @@ require 'yaml'
 module CommonVariables
   #Opencart url
   URL = 'http://new-website.zzz.com.ua/index.php?route=account/register'
-
   #user_credentials
-  CREDENTIALS = YAML.load_file('./setting_examples/credentials.yml')
+  CREDENTIALS = YAML.load_file( "./ruby/selenium/setting_examples/credentials.yml")
 end
